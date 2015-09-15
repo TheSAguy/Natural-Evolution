6de8407004fbf8c353a72ad501da115443b397b3
TheSAguy <vvwmisc@hotmail.com>
2015-09-14 19:27:48 -0700

    9.14 - Fix Bob's spawner
    9.14 - Fix Bob's spawner

--- prototypes/Vanilla_Changes/Bobs_Spawners.lua
+++ prototypes/Vanilla_Changes/Bobs_Spawners.lua
@@ -10,19 +10,17 @@ data.raw["unit-spawner"]["bob-biter-spawner"].result_units = (function()
 					   res[6] = {"medium-biter-Mk3", 	{{0.50, 0.0}, {0.80, 0.3}, {0.90, 0.0}}}
 					   
 					   res[7] = {"big-biter",	 		 {{0.60, 0.0}, {0.90, 0.3}, {1.00, 0.0}}}
-                       res[8] = {"big-biter-Mk2",		 {{0.70, 0.0}, {1.00, 0.1}}}
-					   res[9] = {"big-biter-Mk3", 		 {{0.80, 0.0}, {1.00, 0.1}}}
-
-					   res[10] = {"bob-bigger-biter",  	{{0.70, 0.0}, {0.90, 0.3}}}		
-					   res[11] = {"bob-biggest-biter",	{{0.90, 0.0}, {1.00, 0.3}}}		
-					   res[12] = {"bob-fire-biter",  	{{0.90, 0.0}, {1.00, 0.2}}}		
 					   
-					   res[13] = {"behemoth-biter",  	{{0.90, 0.0}, {1.00, 0.3}}}					   			   
+                       res[8] = {"bob-bigger-biter",  	{{0.70, 0.0}, {1.00, 0.1}}}	
+					   res[9] = {"bob-biggest-biter",	 {{0.80, 0.0}, {1.00, 0.2}}}	
+					   res[10] = {"bob-fire-biter",  	{{0.90, 0.0}, {1.00, 0.3}}}		
+					   
+					   res[11] = {"behemoth-biter",  	{{0.95, 0.0}, {1.00, 0.2}}}					   			   
                     
                      return res
                    end)()
 				   
-	
+
 data.raw["unit-spawner"]["bob-spitter-spawner"].result_units = (function()
                      local res = {}
 
@@ -37,15 +35,13 @@ data.raw["unit-spawner"]["bob-spitter-spawner"].result_units = (function()
 					   res[7] = {"medium-spitter-Mk3", 	{{0.55, 0.0}, {0.85, 0.3}, {0.95, 0.0}}}
 					   
 					   res[8] = {"big-spitter",	 	 	{{0.65, 0.0}, {0.95, 0.3}, {1.00, 0.0}}}
-                       res[9] = {"big-spitter-Mk2",	 	{{0.75, 0.0}, {1.00, 0.1}}}
-					   res[10] = {"big-spitter-Mk3", 	{{0.85, 0.0}, {1.00, 0.1}}}
-					    			                       					   
-					   res[11] = {"bob-bigger-spitter", {{0.70, 0.0}, {0.09, 0.3}}}
-					   res[12] = {"bob-biggest-spitter",{{0.90, 0.0}, {1.00, 0.3}}}
-					   res[13] = {"bob-poison-spitter",	{{0.90, 0.0}, {1.00, 0.2}}}
 					   
-					   res[14] = {"behemoth-spitter",  	{{0.95, 0.0}, {1.00, 0.3}}}				
+                       res[9] = {"bob-bigger-spitter", {{0.75, 0.0}, {1.00, 0.1}}}
+					   res[10] = {"bob-biggest-spitter",{{0.85, 0.0}, {1.00, 0.2}}}
+					   res[11] = {"bob-poison-spitter",	{{0.90, 0.0}, {1.00, 0.3}}}
+					   
+					   res[12] = {"behemoth-spitter",  	{{0.95, 0.0}, {1.00, 0.2}}}				
 					   
                      return res
                    end)()
-				   				   
+				   
